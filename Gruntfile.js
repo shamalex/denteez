f16a894415539155bee0dc38ffccd45fe398bbaf
Shamshin alex <shamshin@i.ua>
2016-01-09 19:24:28 +0300

    +final

---
+++ Gruntfile.js
@@ -0,0 +1,38 @@
+//Стандартный экспорт модуля в nodejs
+module.exports = function(grunt) {
+  // Инициализация конфига GruntJS
+  grunt.initConfig({
+
+    //Настройки различных модулей GruntJS, их нужно предварительно установить через менеджер пакетов npm, или добавить в файл package.json перед запуском npm install
+
+    //Склеивание файлов
+    concat: {
+                dist: {
+                    src: [
+                        'bower_components/jquery/dist/jquery.js',
+                        'bower_components/angular/angular.js',
+                        'bower_components/angular-cookie/angular-cookie.js',
+                        'bower_components/angular-resource/angular-resource.js',
+                        'bower_components/bootstrap-sass/assets/javascripts/bootstrap.js',
+                        'js/app.js'
+                    ],
+                    dest: 'js/production.js',
+                }
+            },
+    uglify: {
+        build: {
+            src: 'js/production.js',
+            dest: 'js/production.min.js'
+        }
+    }
+    //И так далее
+  });
+
+  //Загрузка модулей, которые предварительно установлены
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+grunt.loadNpmTasks('grunt-contrib-concat');
+
+  //Эти задания будут выполнятся сразу же когда вы в консоли напечатание grunt, и нажмете Enter
+grunt.registerTask('default', ['concat', 'uglify']);
+};
